a693e3a1a576a7620b7a30084ad13481a72072b7
dashengz <contact@dashengz.com>
2017-04-12 18:43:03 -0400

    Minor fix

--- app/src/main/java/com/dashengz/inclassassignment10_dashengz/ListActivity.java
+++ app/src/main/java/com/dashengz/inclassassignment10_dashengz/ListActivity.java
@@ -59,26 +59,14 @@ public class ListActivity extends AppCompatActivity {
                 posts.add(post);
                 mAdapter.notifyDataSetChanged();
             }
-
             @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {}
             @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
+            public void onChildRemoved(DataSnapshot dataSnapshot) {}
             @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
             @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
+            public void onCancelled(DatabaseError databaseError) {}
         });
     }
 
